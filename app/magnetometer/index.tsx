28a57b35c771911b1cfd149d37be5c12769eda14
AlejandroEN <alejandroescobarnunez@gmail.com>
2024-10-27 13:48:45 -0500

    feat: Add .prettierrc configuration file and set tab width to 2 spaces with tabs
    - Remove unused useColorScheme hook
    - Update tsconfig.json to exclude expo-env.d.ts from compilation.
    - Update .gitignore to ignore expo-env.d.ts file
    - Add HomeScreen component.
    - Add MagnetometerScreen component.
    - Add AccelerometerScreen component.
    - Add PedometerScreen component.
    - Remove ThemedText-test.tsx file.
    - Update dependencies in package.json.

---
+++ app/magnetometer/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function MagnetometerScreen() {
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Text>Magnetometer Screen</Text>
+		</View>
+	);
+}
